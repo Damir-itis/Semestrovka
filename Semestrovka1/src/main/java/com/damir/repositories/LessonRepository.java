880ae660e268fc7211333402561e1283aee2f79d
DAMIR KASIMOV <kasimov2002@outlook.com>
2022-05-13 23:21:16 +0300

    add gitignore

--- Semestrovka1/src/main/java/com/damir/repositories/LessonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.damir.repositories;
-
-import com.damir.models.Lesson;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LessonRepository extends JpaRepository<Lesson, Long> {
-}
