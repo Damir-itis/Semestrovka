3fa17b3eb2086df72e54b3131a4d1428cb34681d
DAMIR KASIMOV <kasimov2002@outlook.com>
2022-05-13 23:25:33 +0300

    update

--- Semestrovka1/src/main/java/com/damir/repositories/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.damir.repositories;
-
-
-import com.damir.models.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository  extends JpaRepository<Course, Long> {
-
-}
